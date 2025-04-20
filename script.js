520010c1aa2ef4c57d7f07b1673a47744f3fe6d3
Gogineni Deekshit <goginenideekshit3105@gmail.com>
2025-04-20 17:35:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Scroll animations
+const sections = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+sections.forEach(section => {
+  observer.observe(section);
+});
